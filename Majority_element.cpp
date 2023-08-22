26d12a44af7953c75d21f324cb74a9a32cb388fb
Sayan Das <142220476+sayandas-sd@users.noreply.github.com>
2023-08-22 20:36:59 +0530

    Majority_element.cpp

--- Majority_element.cpp
+++ Majority_element.cpp
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
 
 int func(int v[],int n){
         int sum = 0; int maxi = INT_MIN;
@@ -14,18 +12,6 @@ int func(int v[],int n){
         }
         return maxi;
 }
-//main funtion
-int main()
-{   
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    //code
-    
-    int v[]= {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-   int n = sizeof(v) / sizeof(v[0]);
-    cout<<func(v,n);
-     
-}
+
+
 
